3d375c028f189d0d13dc6bbfd06596b5d881d51b
Jammi Dee <jammi_dee@yahoo.com>
2013-06-23 09:05:06 +0800

    Revert "More new codes for security manager"
    This reverts commit 322cd1d8cfa943a5b4ac286ca0d34fa783e6da51.

--- weSecurityManager/webapproot/pages/Main/Main.widgets.js
+++
@@ -1,8 +0,0 @@
-Main.widgets = {
-	layoutBox1: ["wm.Layout", {"_classes":{"domNode":["wm_BackgroundColor_White"]},"customGetValidate":"","height":"100%","horizontalAlign":"center","verticalAlign":"middle","width":"100%"}, {}, {
-		picture1: ["wm.Picture", {"border":"0","height":"59px","source":"resources/images/imagelists/loadingblue.gif","width":"226px"}, {}],
-		label2: ["wm.Label", {"_classes":{"domNode":["wm_FontFamily_Courier","wm_FontSizePx_14px","wm_FontColor_SteelBlue"]},"align":"center","border":"0","caption":"Please wait for a while...","padding":"4","width":"251px"}, {}, {
-			format: ["wm.DataFormatter", {}, {}]
-		}]
-	}]
-}
--- weSecurityManager/webapproot/pages/PgWaitLoadPage/PgWaitLoadPage.widgets.js
+++
@@ -1,5 +0,0 @@
-PgWaitLoadPage.widgets = {
-	layoutBox1: ["wm.Layout", {"_classes":{"domNode":["wm_BackgroundColor_White"]},"customGetValidate":"","horizontalAlign":"center","verticalAlign":"middle"}, {}, {
-		pic_loading: ["wm.Picture", {"height":"43px","source":"resources/images/imagelists/loading3d.gif","width":"226px"}, {}]
-	}]
-}
